cc3b0f0b99e68790aafa41a73ad6f6f7c6b21613
Alejandro Gonzalez Vergara <andresg.vergara04@gmail.com>
2026-05-19 23:43:58 -0400

    un link

--- src/components/Especialidades.jsx
+++ src/components/Especialidades.jsx
@@ -79,7 +79,7 @@ const areas = [
 
 export const Especialidades = () => {
   return (
-    <section className="py-12 bg-transparent interface-object">
+    <section id="especialidades" className="py-12 bg-transparent interface-object">
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-3xl font-bold text-white mb-4">
